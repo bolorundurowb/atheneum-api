9fe998f813e056c344ed1acc2750927a00a8241e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-12-26 18:44:06 +0100

    ft:  add in auth and users scaffold

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DB_URL)
+    MongooseModule.forRoot(process.env.DB_URL),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {
 }
