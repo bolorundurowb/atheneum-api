d410734a259510e0165c8abb1aa67aa66426fc80
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-19 18:54:06 +0100

    ft: add the shared module into the app module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,4 +1,13 @@
 /**
  * Created by bolorundurowb on 6/19/2021
  */
-  
+
+import { Module } from '@nestjs/common';
+import { EmailService } from './services/email.service';
+
+@Module({
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class SharedModule {
+}
