eba16afead29f917d29d3b1bcf144019a36956ba
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-06-23 16:13:29 +0100

    ft: annotate the auth dto

--- src/auth/dtos/auth.dto.ts
+++ src/auth/dtos/auth.dto.ts
@@ -2,8 +2,15 @@
  * Created by bolorundurowb on 1/3/2021
  */
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class AuthDto {
+  @ApiProperty()
   fullName: string;
+
+  @ApiProperty()
   authToken: string;
+
+  @ApiProperty()
   emailAddress: string;
 }
