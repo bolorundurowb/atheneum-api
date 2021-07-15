9e8d1435c56e34c292303f359fd1187c7f4c64b6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-15 11:10:32 +0100

    ft: add publisher service method

--- src/books/services/publisher.service.ts
+++ src/books/services/publisher.service.ts
@@ -6,19 +6,31 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Publisher, PublisherDocument } from '../schemas/publisher.schema';
 import { Model } from 'mongoose';
+import { Book, BookDocument } from '../schemas/book.schema';
 
 @Injectable()
 export class PublisherService {
   constructor(
     @InjectModel(Publisher.name)
     private publisherModel: Model<PublisherDocument>,
+    @InjectModel(Book.name)
+    private bookModel: Model<BookDocument>
   ) {}
 
   async getAll(ownerId: any): Promise<Array<any>> {
     return this.publisherModel
       .find({
-        owner: ownerId,
+        owner: ownerId
       })
       .sort({ name: 'asc' });
+  }
+
+  async getTop(ownerId: any): Promise<Array<any>> {
+    const result = await this.bookModel.aggregate([
+      { $match: { owner: ownerId } },
+      { $group: { _id: '$owner' } },
+      { $project: { publisherId: '$_id' } }
+    ]);
+    return result;
   }
 }
