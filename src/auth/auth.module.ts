f8ca119be859eaca4d19bce2b043c532e4370093
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-07-27 09:20:21 +0100

    ft: add the google client lib and update the auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './controllers/auth.controller';
@@ -7,20 +7,22 @@ import { UsersModule } from '../users/users.module';
 import { AuthService } from './services/auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { SharedModule } from '../shared/shared.module';
+import { SocialAuthService } from './services/social-auth.service';
 
 @Module({
   imports: [
+    HttpModule,
     UsersModule,
     SharedModule,
     PassportModule,
     JwtModule.registerAsync({
       useFactory: async () => ({
-        secretOrPrivateKey: configuration().auth.secret,
+        secret: configuration().auth.secret,
         signOptions: { expiresIn: '365d' }
       })
     })
   ],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, SocialAuthService],
   controllers: [AuthController],
   exports: [PassportModule, JwtModule]
 })
